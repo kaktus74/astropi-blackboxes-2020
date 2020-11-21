9c77ec406175e7df45467de58d3e06cb1c03ff9b
kaktus <grzegorz.zadrozny@zadrozni.eu>
2020-11-21 15:14:52 +0000

    csv random fixed

--- spotkanie-2020-11-21/src/random-csv.py
+++ spotkanie-2020-11-21/src/random-csv.py
@@ -8,16 +8,19 @@ csv_sciezka = '/home/kaktus74/astropi2021/astropi-blackboxes-2020/spotkanie-2020
 #TODO-5 Dodaj do pliku CSV kolejną kolumnę: losowe_imie ktorej wartość będzie losowo wybierana spośród Waszych imion
 with open(csv_sciezka, 'w') as plik:
     #FIXME: użyj znaków cytujących. Patrz: https://docs.python.org/3/library/csv.html
-    writer = csv.writer(plik)
+    writer = csv.writer(plik,delimiter='.', quoting=csv.QUOTE_MINIMAL )
 
     naglowek = ("lp", "calkowita","ulamek")
+    # A B
+    # 1,"Oni,my ,wy"
+    # 2,Ala ma kota
 
     writer.writerow(naglowek)
     for i in range(1,100):
         calkowita = random.randint(0,100)
         ulamek = random.random()
         #FIXME: wyswietlaj tylko 2 miejsca dziesietne
-        wiersz = (i+1, calkowita,ulamek)
+        wiersz = (i+1, calkowita,"{0:.2f}".format(ulamek))
         writer.writerow(wiersz)
 
         
