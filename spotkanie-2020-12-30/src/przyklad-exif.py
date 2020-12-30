0285d064dec1d523c07e77a7acd16825d6e73328
kaktus <grzegorz.zadrozny@zadrozni.eu>
2020-12-30 11:43:41 +0100

    Prca na spotkaniu 2020-12-30

--- spotkanie-2020-12-30/src/przyklad-exif.py
+++ spotkanie-2020-12-30/src/przyklad-exif.py
@@ -9,9 +9,11 @@ camera = PiCamera()
 
 with camera:
     camera.resolution = (1296,972)
-    camera.exif_tags['GPS.GPSLatitude'] = "44/1,45/1,123/10"
+    camera.exif_tags['GPS.GPSLatitude'] = "53/1,25/1,280/10"
     camera.exif_tags['GPS.GPSLatitudeRef'] = "N"
-    camera.exif_tags['GPS.GPSLongitude'] = "15/1,0/1,0/10"
+    camera.exif_tags['GPS.GPSLongitude'] = "14/1,28/1,566/10"
     camera.exif_tags['GPS.GPSLongitudeRef'] = "E"
+    #camera.exif_tags['bez sensu'] = "ddasdas"
     camera.capture(os.path.dirname(__file__) + "/../data/exif.jpg")
+# 53.424449, 14.482395 = 53°25'28.0"N 14°28'56.6"E
 
