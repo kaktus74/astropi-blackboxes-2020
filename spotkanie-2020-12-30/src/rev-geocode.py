346d021d8ed3ab30dd49942a3f9e9e53e139aa2c
kaktus <grzegorz.zadrozny@zadrozni.eu>
2021-01-01 20:53:53 +0100

    Praca na spotkaniu 2020-12-30

--- spotkanie-2020-12-30/src/rev-geocode.py
+++ spotkanie-2020-12-30/src/rev-geocode.py
@@ -1,16 +1,20 @@
 import reverse_geocoder as rg
 from ephem import readtle, degree
 from time import sleep
+from datetime import datetime, timezone
 
 
 name = "ISS (ZARYA)"
-line1 = "1 25544U 98067A   20316.41516162  .00001589  00000+0  36499-4 0  9995"
-line2 = "2 25544  51.6454 339.9628 0001882  94.8340 265.2864 15.49409479254842"
+line1 = "1 25544U 98067A   20365.25294914  .00000870  00000-0  23743-4 0  9990"
+line2 = "2 25544  51.6464  98.3330 0001208 167.2107 217.0739 15.49236186262418"
 
 iss = readtle(name, line1, line2)
-iss.compute()
+now_utc = datetime.now(timezone.utc)
+iss.compute(now_utc)
 
-pos = (54.0, 15.0)
+pos = (iss.sublat / degree, iss.sublong / degree)
+
+#pos = (69.358026, 88.136941)
 
 location = rg.search(pos, mode=1)
-print(location)
+print(location[0]['name'])
