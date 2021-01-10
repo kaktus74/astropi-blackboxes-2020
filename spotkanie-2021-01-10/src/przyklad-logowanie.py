4fbb3d74be6986c79935a5ae1287d98d105cae18
kaktus <grzegorz.zadrozny@zadrozni.eu>
2021-01-10 11:13:46 +0100

    Praca na spotkaniu 20201-01-10: przyklad logowania

---
+++ spotkanie-2021-01-10/src/przyklad-logowanie.py
@@ -0,0 +1,23 @@
+from logzero import logger, logfile, loglevel
+
+
+
+loglevel(50)
+
+def foo():
+
+
+    
+    logfile("/home/kaktus74/nasze_logi.log")
+
+    for i in range(1,100):
+
+        logger.debug("SUper szczegolowe informacje")
+        logger.info("Cos siw wydarzylo!")
+        logger.warning("Chyba cos sie wywrocilo")
+        logger.error("Oj, cos sie na pewno wywrocilo!")
+        rok = 19
+        logger.critical(f"Wykryto wirusa covid-{rok}! {i}")
+
+if __name__ == '__main__':
+    foo()
