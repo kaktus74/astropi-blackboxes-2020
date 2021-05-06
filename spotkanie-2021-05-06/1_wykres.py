81e25a874371969161725af40614a9eeba21dce6
kaktus <kaktus74@gmail.com>
2021-05-06 18:01:03 +0200

    Spotkanie 2021-05-06

---
+++ spotkanie-2021-05-06/1_wykres.py
@@ -0,0 +1,37 @@
+from csv import reader
+
+with open('oceny.csv','r') as f:
+    data = list(reader(f))
+    #for row in reader(f):
+        #print(row)
+    #it = reader(f)
+    #row = next(it)
+    #print(row)
+    
+    #row = next(it)
+    #print(row[0])
+    #print(row[1])
+    
+only_data = data[1:]
+
+osoby = [ row[0] for row in only_data]
+oceny = [ row[1] for row in only_data]
+
+
+#print(osoby)
+#print(oceny)
+
+print(type(data))
+
+
+import matplotlib 
+
+
+from matplotlib import pyplot
+
+pyplot.plot(osoby,oceny)
+pyplot.xlabel('Osoby')
+pyplot.ylabel('Ocena')
+pyplot.title('Oceny')
+
+pyplot.show()
