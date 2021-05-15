612a506cf3b3ca22edd082257f8ea99d3f0c9021
kaktus <grzegorz.zadrozny@zadrozni.eu>
2021-05-15 13:59:08 +0200

    Przygotowanie spotkania 2021-05-15: szum i wielokrotne diagramy

--- spotkanie-2021-05-07/kills_covid_rus_1.py
+++ spotkanie-2021-05-07/kills_covid_rus_1.py
@@ -5,10 +5,12 @@ from matplotlib import pyplot
 with open("covid.csv",'r') as f:
     tanea = [dane for dane in list(reader(f)) if dane[0] == 'RUS']
     time = [parser.parse (data[3]) for data in tanea]
-    kills_total      = [int (cases[7]) for cases in tanea]
+    kills_total      = 
     killstreak_daily = [int (cases[8]) for cases in tanea]
+    
 
     pyplot.ylabel('covid russia is in trouble')
     pyplot.plot(time, kills_total)
     pyplot.plot(time, killstreak_daily)
     pyplot.show()
+
--- spotkanie-2021-05-07/matoszka_rasija.py
+++ spotkanie-2021-05-07/matoszka_rasija.py
@@ -8,6 +8,8 @@ with open("covid.csv",'r') as f:
     y_axis1 = [float (cases[10]) for cases in rusispoaczn_data]
     y_axis2 = [float (cases[11]) for cases in rusispoaczn_data]
 
+    fig, ax = pyplot.subplots(2)
+
     pyplot.ylabel('Zachorowania na 1 000 000 ludzi.')
-    pyplot.plot(time, y_axis1)
+    ax[0].plot(time, y_axis1)
     pyplot.show()
